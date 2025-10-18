a179934533bec1ab1e19793828a7652b34ebaa85
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-10-18 23:51:44 +0900

    feat: meta tags integration on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log(" DOCUMENT PAGE Router");
-
   return (
     <Html lang="en">
+      <meta charSet="UTF-8" />
+      <meta name="title" content="Nestar Demo" />
+      <meta name="robots" content="index,follow" />
+      <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+      {/* SEO */}
+      <meta
+        name="keyword"
+        content={"nestar, nestar.uz, devex mern , mern nestjs fullstack"}
+      />
+      <meta
+        name="description"
+        content={
+          "Buy and sell properties anywere anytime in South Korea. |" +
+          "Купить и продать недвижимость были в любое время в Южной Корее. |" +
+          "한국에서는 언제든지 부동산을 사고팔 수 있습니다."
+        }
+      />
+
       <Head />
       <body>
         <Main />
