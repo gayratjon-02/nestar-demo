e0e162edf118e802d292da2182d06ba887678d6c
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-10-21 17:47:13 +0900

    feat: publish main layout components
    ent

--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Stack } from "@mui/material";
 import Footer from "../Footer";
 import Top from "../Top";
+import HeaderFilter from "../homepage/HeaderFilter";
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
@@ -14,7 +15,11 @@ const withLayoutMain = (Component: any) => {
           <Stack id={"top"}>
             <Top />
           </Stack>
-          <Stack className={"header-main"}>Header Filter</Stack>
+          <Stack className={"header-main"}>
+            <Stack className="container">
+                <HeaderFilter/>
+              </Stack>            
+            </Stack>
 
           <Stack id={"main"}>
             <Component {...props} />
